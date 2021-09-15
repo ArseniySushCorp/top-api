981adda64449dded48c48635e45011532e27b02c
Arseniy Suschevskiy <a.suschevskiy@voiso.com>
2021-09-15 21:56:16 +0300

    review service

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,14 +1,45 @@
+import { ReviewService } from './review.service';
+import { CreateReviewDto } from './dto/create-review.dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { ReviewModel } from './review.model';
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { ModelType, DocumentType } from '@typegoose/typegoose/lib/types';
 
 @Controller('review')
 export class ReviewController {
+  constructor(private readonly reviewService: ReviewService) {}
+
   @Post()
-  async create(@Body() dto: Omit<ReviewModel, '_id'>) {}
+  async create(
+    @Body() dto: CreateReviewDto,
+  ): Promise<DocumentType<ReviewModel>> {
+    return this.reviewService.create(dto);
+  }
 
   @Delete(':id')
-  async delete(@Param('id') id: string) {}
+  async delete(@Param('id') id: string): Promise<DocumentType<ReviewModel>> {
+    const deletedDoc = await this.reviewService.delete(id);
+
+    if (!deletedDoc) {
+      throw new HttpException(
+        'Review with this id not found',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    return deletedDoc;
+  }
 
   @Get('byProduct/:productId')
-  async getByProduct(@Param('id') productId: string) {}
+  async getByProduct(@Param('id') productId: string) {
+    return this.reviewService.findByProductId(productId);
+  }
 }
