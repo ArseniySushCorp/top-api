d4265b323f139e3f5c339f7b6e2e2de685471020
Arseniy Suschevskiy <a.suschevskiy@voiso.com>
2021-09-27 22:23:25 +0300

    add files serve

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -19,7 +19,7 @@ export class FilesController {
   @Post('upload')
   @HttpCode(200)
   @UseGuards(JwtAuthGuard)
-  @UseInterceptors(FileInterceptor('files'))
+  @UseInterceptors(FileInterceptor('file'))
   async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<FileUnitResponse[]> {
     const saveArray: MFile[] = [file];
 
